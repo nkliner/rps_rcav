9f6076af46bc880e645bb51c67358e7c9c83e163
Nick Kliner <nkliner@chicagobooth.edu>
2017-04-20 14:31:33 -0500

    Rock

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get("/rock", {:controller => "games", :action => "play_rock"})
 end
